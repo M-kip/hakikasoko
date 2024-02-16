1d0497a740d339de237866d2e6b9d75640859b4b
M-kip <bettmoseskiprono@gmail.com>
2024-02-16 13:43:34 +0300

    test server vonfig

--- tradingWebsite/settings.py
+++ tradingWebsite/settings.py
@@ -1,8 +1,8 @@
 import os
 from .common_settings import *
 
-DEBUG = False
-DJANGO_VITE_DEV_MODE = False
+DEBUG = True
+DJANGO_VITE_DEV_MODE = DEBUG
 assert SECRET_KEY is not None, ('please provide DJANGO_SECRET_KEY'
                                 'environment variable with value')
 ALLOWED_HOSTS += [
