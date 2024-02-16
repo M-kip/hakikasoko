20ea8b16f2b6188f4e5e397833c327577ecce5b5
M-kip <bettmoseskiprono@gmail.com>
2024-02-16 14:58:58 +0300

    config

--- tradingWebsite/settings.py
+++ tradingWebsite/settings.py
@@ -1,7 +1,7 @@
 import os
 from .common_settings import *
 
-DEBUG = True
+DEBUG = False
 DJANGO_VITE_DEV_MODE = DEBUG
 assert SECRET_KEY is not None, ('please provide DJANGO_SECRET_KEY'
                                 'environment variable with value')
