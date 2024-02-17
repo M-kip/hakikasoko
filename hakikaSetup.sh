27fb84fc772b12ed3a6289e82e4c86f4c1a4c4bd
M-kip <bettmoseskiprono@gmail.com>
2024-02-17 10:51:44 +0300

    automation debug

--- hakikaSetup.sh
+++ hakikaSetup.sh
@@ -1,6 +1,6 @@
 !#/usr/bin/bash
 pushd /home/musa/documents/hakikasoko
-copy uwsgiConfig/uwsgi_hakika.ini /etc/uwsgi/
-copy uwsgiConfig/uswgi.service /etc/systemd/system/
-copy uwsgiConfig/uwsgi_run.sh /usr/local/bin/
+cp uwsgiConfig/uwsgi_hakika.ini /etc/uwsgi/
+cp uwsgiConfig/uswgi.service /etc/systemd/system/
+cp uwsgiConfig/uwsgi_run.sh /usr/local/bin/
 popd
